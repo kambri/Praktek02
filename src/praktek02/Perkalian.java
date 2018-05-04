9a9e52f1e82149bc8e619af308962f17e8791f78
kambri <37506225+kambri@users.noreply.github.com>
2018-05-04 19:37:10 +0700

    Membuat Attribute dan Behavior

--- src/praktek02/Perkalian.java
+++ src/praktek02/Perkalian.java
@@ -1,4 +1,12 @@
 package praktek02;
 public class Perkalian {
+    double angka1;
+    double angka2;
     
+    void cetakInfo(){
+        System.out.println("==================================");
+        System.out.println("Angka Pertama = "+angka1);
+        System.out.println("Angka Kedua   = "+angka2);
+        System.out.println("==================================");
+    }
 }
