b957b820ade1dd0e64316bcc7c3936e1a5e0b885
kambri <37506225+kambri@users.noreply.github.com>
2018-05-04 19:44:04 +0700

    Mengisi nilai attribute

--- src/praktek02/PerkalianAksi.java
+++ src/praktek02/PerkalianAksi.java
@@ -2,5 +2,7 @@ package praktek02;
 public class PerkalianAksi {
     public static void main (String[] args){
         Perkalian n1 = new Perkalian();
+        n1.angka1 = 5;
+        n1.angka2 = 25;
     } 
 }
